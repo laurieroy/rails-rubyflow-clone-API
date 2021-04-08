39b9625d100f4d2a76ee1b455c181cbcf574e48c
lauretteroy <laurieroy.dev@gmail.com>
2021-04-08 16:41:15 -0600

    Add initial article model validaiton, factory, test

---
+++ spec/factories/articles.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :article do
+    sequence(:title) { |n| "Sample article #{n}"} 
+    content { "Sample Content" }
+    sequence(:slug ) { |n| "sample-article-#{n}"}
+  end
+end
