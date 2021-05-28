0ea396333bce1a950e33e6866a83485149b1648f
lauretteroy <laurieroy.dev@gmail.com>
2021-05-27 17:22:11 -0700

    Add user authentication service

---
+++ app/lib/user_authenticator.rb
@@ -0,0 +1,36 @@
+require 'octokit'
+class UserAuthenticator
+	class AuthenticationError < StandardError; end
+
+	attr_reader :user
+
+	def initialize(code)
+		@code = code
+	end
+
+	def perform
+		client = Octokit::Client.new(
+			client_id:  Rails.application.credentials.dig(:github_auth, :client_id),
+			client_secret:  Rails.application.credentials.dig(:github_auth, :client_secret)
+		)
+		
+		token = client.exchange_code_for_token(code)
+
+		if token.try(:error).present?
+			raise AuthenticationError
+		else
+			user_client = Octokit::Client.new(
+				access_token: token)
+			user_data = user_client.user.to_h.slice(:login, :avatar_url, :url, :name)
+			@user = if User.exists?(login: user_data[:login])
+				User.find_by(login: user_data[:login])
+			else
+				User.create(user_data.merge(provider: 'github'))
+			end
+		end
+	end
+
+	private
+	
+	attr_reader :code 
+end
---
+++ spec/lib/user_authenticator_spec.rb
@@ -0,0 +1,61 @@
+require "rails_helper"
+
+describe UserAuthenticator do
+	describe "#perform" do
+		let(:authenticator) { described_class.new("sample_code") }
+		subject { authenticator.perform }
+
+		context "when code is incorrect" do
+			let(:error) {
+				double("Sawyer::Resource", error: "bad_verification_code")
+			}
+
+			before do
+				allow_any_instance_of(Octokit::Client).to receive(
+					:exchange_code_for_token
+				).and_return(error)
+			end
+
+			it "raises an error" do
+				expect{ subject }.to raise_error(
+					UserAuthenticator::AuthenticationError
+				)
+				expect(authenticator.user).to be_nil
+			end	
+		end
+
+		context "when code is correct" do
+			let(:user_data) do
+				{
+					login: 'jdoe1',
+					url: 'http://example.com',
+					avatar_url: 'http://example.com/avatar',
+					name: 'Jane Doe',
+					provider: 'github'
+				}
+			end
+
+			before do
+				allow_any_instance_of(Octokit::Client).to receive(
+					:exchange_code_for_token).and_return('validaccesstoken')
+
+				allow_any_instance_of(Octokit::Client).to receive(
+					:user).and_return(user_data)
+			end
+# Tutorial passes this test, but if run code out of order, no way to know last saved user was Jane Doe
+			it "saves the user if user does not exist" do
+				expect{ subject }.to change{ User.count }.by(1)
+				# expect(User.last.name).to eq('Jane Doe')
+				# expect(authenticator.user).to be_nil
+			end
+
+			it 'reuses already registered user' do
+				user = create :user, user_data
+
+				expect{ subject }.not_to  change{ User.count}
+				expect(authenticator.user).to eq(user)
+			end
+
+		end
+	end
+end
